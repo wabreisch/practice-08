e4c77058bec4c12a4e4a46c1448abe3ea0ed2701
wabreisch <wabreisch@quinnipiac.edu>
2016-04-13 14:10:22 -0400

    Clean up project for submission

--- src/main/java/csc375/Practice_08/problem01/HealthRecord.java
+++ src/main/java/csc375/Practice_08/problem01/HealthRecord.java
@@ -1,4 +1,4 @@
-package csc375.Practice_08;
+package csc375.Practice_08.problem01;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,7 +10,7 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Range;
 import com.google.common.collect.TreeRangeSet;
 
-public class CigaretteConceptionRecord {
+public class HealthRecord {
 
 	private int id;
 	private int doctor;
@@ -18,7 +18,23 @@ public class CigaretteConceptionRecord {
 	private double access;
 	private double health;
 	
-	public CigaretteConceptionRecord(int id, int doctor, int children, double access, double health) {
+	/**
+	  * Constructor.
+	  * 
+	  * Empty constructor for running test cases
+	  */
+    public HealthRecord() {}
+
+    /**
+     * Constructor.
+     * 
+     * @param id (required) 
+     * @param doctor (required) 
+     * @param children (required) 
+     * @param access (required) 
+     * @param health (required) 
+     */
+	public HealthRecord(int id, int doctor, int children, double access, double health) {
 		this.id = id;
 		this.doctor = doctor;
 		this.children = children;
@@ -26,25 +42,30 @@ public class CigaretteConceptionRecord {
 		this.health = health;
 	}
 
-    public static void main( String[] args ) throws IOException {
-    	System.out.println(calculateAverageSize());
-    	accessRange();
-    }
-    
-    public static double calculateAverageSize() throws IOException {
-    	ArrayListMultimap<Integer, Integer> almm = generateMultimap();
-    	List<Integer> al = (List<Integer>) almm.get(4);
-    	int numFamilies = al.size();
+	/**
+	 * 
+	 * @return The average number of doctor visits by a family of four people per year
+	 * @throws IOException
+	 */
+    public double calculateAverageDoctorVisits() throws IOException {
+    	ArrayListMultimap<Integer, Integer> multimap = generateMultimap();
+    	List<Integer> numDoctorVisits = (List<Integer>) multimap.get(4);
+    	int numFamilies = numDoctorVisits.size();
     	int total = 0;
     	
-    	 for (int i : al) {
+    	 for (int i : numDoctorVisits) {
     		 total += i;
     	 }
     	
     	return ((double)total / (double)numFamilies);
     }
     
-    public static ArrayListMultimap<Integer, Integer> generateMultimap() throws IOException {
+    /**
+     * 
+     * @return An ArrayListMultiMap that contains all data rows from the Doctor.csv file
+     * @throws IOException
+     */
+    public ArrayListMultimap<Integer, Integer> generateMultimap() throws IOException {
     	ArrayListMultimap<Integer, Integer> multimap = ArrayListMultimap.create();
     	String currLine;
     	
@@ -52,8 +73,8 @@ public class CigaretteConceptionRecord {
 		while ((currLine = br.readLine()) != null) {
 			String[] splitLine = currLine.split(",");
 		
-			CigaretteConceptionRecord ccr =
-				new CigaretteConceptionRecord(
+			HealthRecord ccr =
+				new HealthRecord(
 					Integer.parseInt(splitLine[0].replaceAll("\"", "")),
 					Integer.parseInt(splitLine[1]),
 					Integer.parseInt(splitLine[2]),
@@ -66,12 +87,20 @@ public class CigaretteConceptionRecord {
     	return multimap;
     }
     
-    public static int accessRange() throws IOException {
+    /**
+     * 
+     * @return an array of integers in which the 0th index holds the number of people with
+     * health coverage less than 0.5 and the 1st index holds the number of people with
+     * health coverage greater than or equal to 0.5.
+     * @throws IOException
+     */
+    public int[] accessRange() throws IOException {
     	TreeRangeSet<Integer> lowerHalf = TreeRangeSet.create();
     	TreeRangeSet<Integer> upperHalf = TreeRangeSet.create();
     	String currLine;
     	int lowerHalfCount = 0;
     	int upperHalfCount = 0;
+    	int[] returnValue = new int[2];
     	
     	lowerHalf.add(Range.open(0, 500));
     	upperHalf.add(Range.closedOpen(500, 1000));
@@ -80,8 +109,8 @@ public class CigaretteConceptionRecord {
 		while ((currLine = br.readLine()) != null) {
 			String[] splitLine = currLine.split(",");
 		
-			CigaretteConceptionRecord ccr =
-				new CigaretteConceptionRecord(
+			HealthRecord ccr =
+				new HealthRecord(
 					Integer.parseInt(splitLine[0].replaceAll("\"", "")),
 					Integer.parseInt(splitLine[1]),
 					Integer.parseInt(splitLine[2]),
@@ -96,12 +125,18 @@ public class CigaretteConceptionRecord {
 			}
 		
 		}
-		System.out.printf("lower: %d, upper: %d\n", lowerHalfCount, upperHalfCount);
-		return 0;
-//    	return lowerHalfCount;
+		
+		returnValue[0] = lowerHalfCount;
+		returnValue[1] = upperHalfCount;
+
+    	return returnValue;
     }
     
-    public static BufferedReader getBufferedReader (String fileName) {
+    /**
+     * @param fileName
+     * @return a BufferedReader instance based on the fileName
+     */
+    public BufferedReader getBufferedReader (String fileName) {
     	File csvFile = new File(fileName);
     	BufferedReader br = null;
     	
---
+++ src/test/java/csc375/Practice_08/problem01test/HealthRecordTest.java
@@ -0,0 +1,29 @@
+package csc375.Practice_08.problem01test;
+
+import java.io.IOException;
+
+import csc375.Practice_08.problem01.HealthRecord;
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for HealthRecord class.
+ */
+public class HealthRecordTest extends TestCase {
+
+    /**
+     * Test average number of doctor visits method
+     * @throws IOException 
+     */
+    public void testMethod1() throws IOException {
+    	assertEquals("Test average doctor visits", (int) (new HealthRecord().calculateAverageDoctorVisits()), 2);
+    }
+    
+    /**
+     * Test both the upper and lower halves of the coverage TreeRangeSet
+     * @throws IOException 
+     */
+    public void testMethod2() throws IOException {
+    	assertEquals("Test coverage range, lower half", (new HealthRecord().accessRange()[0]), 279);
+    	assertEquals("Test coverage range, upper half", (new HealthRecord().accessRange()[1]), 206);
+    }
+}
